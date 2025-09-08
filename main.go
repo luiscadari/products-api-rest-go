0918c9da7bd0dae953ab48f91eef8a8cac1bfd24
luiscadari <luiscadari@icloud.com>
2025-09-08 07:44:23 -0300

    :recycle: refactor: routes and controllers

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 
-	"github.com/luiscadari/products-api-rest-go/models"
+	"github.com/luiscadari/products-api-rest-go/routes"
 )
 
-
-
-
-var templates = template.Must(template.ParseGlob("templates/*html"))
-
 func main() {
-	http.HandleFunc("/", index)
+	routes.GetRoutes()
 	http.ListenAndServe(":8000", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request){
-
-	products := models.GetProducts();
-	templates.ExecuteTemplate(w, "Index", products)
 }
