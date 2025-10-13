1ca222be78a2df201bdc776ea3f6037dc0dcf322
luiscadari <luiscadari@icloud.com>
2025-10-13 14:25:24 -0300

    :packages: build: mod

--- db/db.go
+++ db/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ConnectBD() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=123456 host=localhost sslmode=disable"
+	conexao := "user=postgres dbname=products password=1808 host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 	if err != nil{
 		panic(err.Error())
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/luiscadari/products-api-rest-go/routes"
@@ -8,5 +9,6 @@ import (
 
 func main() {
 	routes.GetRoutes()
+	fmt.Println("Server is listening on port 8000")
 	http.ListenAndServe(":8000", nil)
 }
